8ec7997f1cf12312e252178c937aca331b395e95
William Chandler <chndlr.wllm@gmail.com>
2019-04-16 20:46:33 -0500

    route updates for bands, band

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -6,11 +6,42 @@ let Band = EmberObject.extend({
   name: ""
 });
 
+let Song = EmberObject.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
 export default Route.extend({
   model() {
-    let ledZeppelin = Band.create({ name: "Led Zeppelin" });
-    let pearlJam = Band.create({ name: "Pearl Jam" });
-    let fooFighters = Band.create({ name: "Foo Fighters" });
-    return A([ledZeppelin, pearlJam, fooFighters]);
+    let blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+      });
+      let yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+      });
+      let pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+      });
+      let daughter = Song.create({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+      });
+
+      let ledZeppelin = Band.create({ name: 'Led Zeppelin', songs:
+      A([blackDog]) });
+      let pearlJam = Band.create({ name: 'Pearl Jam', songs:
+      A([yellowLedbetter, daughter]) });
+      let fooFighters = Band.create({ name: 'Foo Fighters', songs:
+      A([pretender]) });
+
+      return A([ledZeppelin, pearlJam, fooFighters]);
   }
 });
