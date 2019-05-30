ca5e4fc7d9f1bb344b5c9de2402d790742509384
William Chandler <chndlr.wllm@gmail.com>
2019-05-29 20:54:59 -0500

    loading state

---
+++ tests/unit/utils/wait-test.js
@@ -0,0 +1,11 @@
+import wait from 'rarwe/utils/wait';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | wait', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = wait();
+    assert.ok(result);
+  });
+});
