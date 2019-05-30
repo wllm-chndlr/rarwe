71aa4e7881ba311a4fdcfc90aef37383d2c9bea8
William Chandler <chndlr.wllm@gmail.com>
2019-05-30 16:27:20 -0500

    capitalize helper

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,9 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-    return this.modelFor('bands.band');
-  },
   resetController(controller) {
     controller.setProperties({
       isAddingSong: false,
