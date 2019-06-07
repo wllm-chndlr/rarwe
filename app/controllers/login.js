bcaefb8fd3fce9b3b0a8496660ac2856f90446e3
William Chandler <chndlr.wllm@gmail.com>
2019-06-06 21:14:51 -0500

    authentication credentials

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,11 +1,14 @@
 import Controller from "@ember/controller";
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  session: service(),
+
   actions: {
     async signIn(event) {
       event.preventDefault();
       let { email, password } = this;
-      //TODO: authenticate the user against the back-end 
+      await this.session.authenticate('authenticator:credentials', email, password);
       await this.transitionToRoute('bands');
     }
   }
--- mirage/config.js
+++ mirage/config.js
@@ -7,4 +7,15 @@ export default function() {
     return schema.songs.where({ bandId: id });
   });
   this.post('/users');
+
+  this.post('/token', function(schema, request) {
+    let { username: email, password } = JSON.parse(request.requestBody);
+    let users = schema.users.where({ email, password });
+    if (users.length === 1) {
+      return {
+        token: 'a.signed.jwt',
+        user_email: email
+      } 
+    }
+  });
 }
