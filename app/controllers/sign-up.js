0a1f75ea73f03f7bc636188461d0cb08b1ef1a6f
William Chandler <chndlr.wllm@gmail.com>
2019-06-27 13:51:14 -0500

    validation for sign-up and login

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,13 +1,21 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+  showErrors: computed('_showErrors', {
+    get() {
+      return this._showErrors || { email: false, password: false };
+    },
+    set(key, value) {
+      this.set('_showErrors', value);
+      return this._showErrors;
+    }
+  }),
   actions: {
     async signUp(event) {
       event.preventDefault();
-      let { email, password } = this;
-      let user = this.store.createRecord('user', { email, password });
-      await user.save();
+      await this.model.save();
       await this.transitionToRoute('login');
-    } 
+    }
   }
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,34 @@
-import DS from 'ember-data';
+import DS from "ember-data";
+import { buildValidations, validator } from "ember-cp-validations";
 
-export default DS.Model.extend({
-  email: DS.attr('string'),
-  password: DS.attr('string'),
+const { Model, attr } = DS;
+
+const Validations = buildValidations({
+  email: [
+    validator("presence", {
+      presence: true,
+      ignoreBlank: true,
+      message: "Email can't be empty"
+    }),
+    validator("format", {
+      type: "email",
+      message: "Email should be a valid email"
+    })
+  ],
+  password: [
+    validator("presence", {
+      presence: true,
+      ignoreBlank: true,
+      message: "Password can't be empty"
+    }),
+    validator("length", {
+      min: 8,
+      message: "Password should be at least 8 characters"
+    })
+  ]
+});
+
+export default Model.extend(Validations, {
+  email: attr("string"),
+  password: attr("string")
 });
