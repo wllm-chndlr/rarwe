ec5658b6cbc03379f0b2e2756097d9246f37759d
William Chandler <chndlr.wllm@gmail.com>
2019-07-15 20:43:32 -0500

    deployyy

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,10 @@
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import DS from 'ember-data';
+import ENV from 'rarwe/config/environment';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  host: ENV.apiHost,
+
   authorize(xhr) {
     let { token } = this.get('session.data.authenticated');
     if (token) {
--- app/authenticators/credentials.js
+++ app/authenticators/credentials.js
@@ -1,11 +1,13 @@
 import Base from 'ember-simple-auth/authenticators/base';
 import { inject as service } from '@ember/service';
+import ENV from 'rarwe/config/environment';
 
 export default Base.extend({
   ajax: service(),
   
   async authenticate(username, password) {
-    let response = await this.ajax.post('/token', {
+    let tokenURL = ENV.apiHost ? `${ENV.apiHost}/token` : '/token';
+    let response = await this.ajax.post(tokenURL, {
       headers: {
         'Accept': 'application/vnd.api+json',
         'Content-Type': 'application/vnd.api+json'
