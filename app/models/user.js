5cb7dbd767a59ba99415507995589d4b0358a083
William Chandler <chndlr.wllm@gmail.com>
2019-07-16 20:23:37 -0500

    user model update

--- app/models/user.js
+++ app/models/user.js
@@ -10,31 +10,6 @@ const Validations = buildValidations({
 
 const { Model, attr } = DS;
 
-// const Validations = buildValidations({
-//   email: [
-//     validator("presence", {
-//       presence: true,
-//       ignoreBlank: true,
-//       message: "Email can't be empty"
-//     }),
-//     validator("format", {
-//       type: "email",
-//       message: "Email should be a valid email"
-//     })
-//   ],
-//   password: [
-//     validator("presence", {
-//       presence: true,
-//       ignoreBlank: true,
-//       message: "Password can't be empty"
-//     }),
-//     validator("length", {
-//       min: 8,
-//       message: "Password should be at least 8 characters"
-//     })
-//   ]
-// });
-
 export default Model.extend(Validations, {
   email: attr("string"),
   password: attr("string")
