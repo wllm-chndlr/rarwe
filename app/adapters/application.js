301d99db8ba40361c480ded050ab4eafc43ac900
William Chandler <chndlr.wllm@gmail.com>
2019-07-17 17:13:23 -0500

    adapter update

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,12 +3,12 @@ import DS from 'ember-data';
 import ENV from 'rarwe/config/environment';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: ENV.apiHost,
+    host: ENV.apiHost,
 
-  authorize(xhr) {
-    let { token } = this.get('session.data.authenticated');
-    if (token) {
-      xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+    authorize(xhr) {
+        let { token } = this.get('session.data.authenticated');
+        if (token) {
+            xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+        }
     }
-  }
 });
